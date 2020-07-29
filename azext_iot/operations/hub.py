8413fb186f5199624f7cd0e298f2ba90404f05f7
sree <sree@laptop-D64A5CF-1.fareast.corp.microsoft.com>
2020-07-29 11:29:42 +0530

    wip

--- azext_iot/operations/hub.py
+++ azext_iot/operations/hub.py
@@ -4,7 +4,6 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-import pdb
 from os.path import exists, basename
 from time import time, sleep
 import six
